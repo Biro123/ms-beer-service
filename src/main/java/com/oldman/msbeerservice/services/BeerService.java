6902be3d2a05090a39a08e39aa965b35ee39f0d2
Biro123 <biro123@ntlworld.com>
2020-12-11 16:20:51 +0000

    Add new listBeers endpoint

--- src/main/java/com/oldman/msbeerservice/services/BeerService.java
+++ src/main/java/com/oldman/msbeerservice/services/BeerService.java
@@ -1,12 +1,20 @@
 package com.oldman.msbeerservice.services;
 
 import com.oldman.msbeerservice.web.model.BeerDto;
+import com.oldman.msbeerservice.web.model.BeerPagedList;
+import com.oldman.msbeerservice.web.model.BeerStyleEnum;
+import org.springframework.data.domain.PageRequest;
+
 import java.util.UUID;
 
 public interface BeerService {
+
+    BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest);
+
     BeerDto getById(UUID beerId);
 
     BeerDto saveNewBeer(BeerDto beerDto);
 
     BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+
 }
--- src/main/java/com/oldman/msbeerservice/services/BeerServiceImpl.java
+++ src/main/java/com/oldman/msbeerservice/services/BeerServiceImpl.java
@@ -5,10 +5,16 @@ import com.oldman.msbeerservice.repositories.BeerRepository;
 import com.oldman.msbeerservice.web.controller.NotFoundException;
 import com.oldman.msbeerservice.web.mappers.BeerMapper;
 import com.oldman.msbeerservice.web.model.BeerDto;
+import com.oldman.msbeerservice.web.model.BeerPagedList;
+import com.oldman.msbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +22,34 @@ public class BeerServiceImpl implements BeerService {
 
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
+
+
+    @Override
+    public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest) {
+        BeerPagedList beerPagedList;
+        Page<Beer> beerPage;
+
+        if (!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
+            // search both
+            beerPage = beerRepository.findAllByBeerNameAndBeerStyle(beerName, beerStyle, pageRequest);
+        } else if (!StringUtils.isEmpty(beerName) && StringUtils.isEmpty(beerStyle)) {
+            beerPage = beerRepository.findAllByBeerName(beerName, pageRequest);
+        } else if (StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
+            beerPage = beerRepository.findAllByBeerStyle(beerStyle, pageRequest);
+        } else {
+            beerPage = beerRepository.findAll(pageRequest);
+        }
+
+        beerPagedList = new BeerPagedList(beerPage
+                .getContent()
+                .stream()
+                .map(beerMapper::beerToBeerDto)
+                .collect(Collectors.toList()),
+                PageRequest.of(beerPage.getPageable().getPageNumber(), beerPage.getPageable().getPageSize()),
+                beerPage.getTotalElements());
+
+        return beerPagedList;
+    }
 
     @Override
     public BeerDto getById(UUID beerId) {
